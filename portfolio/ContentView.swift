20387772e9f8bc7cd0dfba20e67d8579522de3ef
ZeroZ9 <tranlonguuu@gmail.com>
2024-03-17 03:12:01 -0400

    Initial Commit

---
+++ portfolio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  portfolio
+//
+//  Created by Long Tran M2 on 17/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ portfolio/portfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  portfolioApp.swift
+//  portfolio
+//
+//  Created by Long Tran M2 on 17/3/24.
+//
+
+import SwiftUI
+
+@main
+struct portfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
